22d0fd7df97e4ac4f5c6d2d6b81e333f123806cf
ChristianAG <christianandreigonzaga26@gmail.com>
2026-09-05 12:50:58 +0800

    Import and connect all routes in server.js
    Updated server.js to import and connect all routes for customers, appointments, technicians, services, and service records.

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express();
 
 app.use(express.json());
 
-// Import customer routes
+// Import ALL routes
 const customerRoutes = require('./routes/customers');
+const appointmentRoutes = require('./routes/appointments');
+const technicianRoutes = require('./routes/technicians');
+const serviceRoutes = require('./routes/services');
+const serviceRecordRoutes = require('./routes/serviceRecords');
+
+// Connect ALL routes
 app.use('/customers', customerRoutes);
+app.use('/appointments', appointmentRoutes);
+app.use('/technicians', technicianRoutes);
+app.use('/services', serviceRoutes);
+app.use('/service-records', serviceRecordRoutes);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
